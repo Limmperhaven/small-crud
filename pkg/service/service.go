5242f3c85559941e19c9995e421e896ad9114c67
Artem(Boss of the Gym) <aachulaevskiy@miem.hse.ru>
2022-04-27 12:11:59 +0300

    pg-init

---
+++ pkg/service/service.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"gitlab.digital-spirit.ru/study/artem_crud/models"
+	"gitlab.digital-spirit.ru/study/artem_crud/pkg/repository"
+)
+
+type Record interface {
+	GetById(recordUid string) (models.Record, error)
+	GetByFilter(params models.RecordInput) ([]models.Record, error)
+	Create(record models.Record) (string, error)
+	Update(recordUid string, input models.RecordInput) error
+	Delete(recordUid string) error
+}
+
+type Service struct {
+	Record
+}
+
+func NewService(repo *repository.Repository) *Service {
+	return &Service{
+		Record: NewRecordService(repo),
+	}
+}
